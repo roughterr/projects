099746ed455b8bc3e47c85a098db97c79034564e
roughterr <roughterr@gmail.com>
2015-09-19 01:54:43 +0300

    corrected the description of an interface

--- lab1/src/org/nau/VectorDataStructureIntf.java
+++ lab1/src/org/nau/VectorDataStructureIntf.java
@@ -2,7 +2,7 @@ package org.nau;
 
 /**
  * This interface is based on data from the lab#1. It's applicable to all the variants.
- *
+ * Структура даних з векторним способом розміщення елементів.
  * @author Roman.Pikalyuk
  */
 public interface VectorDataStructureIntf {
